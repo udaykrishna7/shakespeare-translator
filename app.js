3e9cb9381f7cc1b9d259ac9b5ab109bd419e8249
Uday Krishna <uday.krishna10000@gmail.com>
2020-12-14 17:24:44 +0530

    initial commit
    basic input output

---
+++ app.js
@@ -0,0 +1,7 @@
+var txtInput = document.querySelector("#input-text");
+var translateBtn = document.querySelector("btn-translate");
+var txtOutput = document.querySelector("output-text");
+
+function clickHandler() {
+    
+}
