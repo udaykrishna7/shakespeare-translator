4e78bbfb9bb109312021496c97785a29816d9f4b
Uday Krishna <uday.krishna10000@gmail.com>
2020-12-15 09:20:04 +0530

    translation done

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var txtInput = document.querySelector("#input-text");
-var translateBtn = document.querySelector("btn-translate");
-var txtOutput = document.querySelector("output-text");
+var translateBtn = document.querySelector("#btn-translate");
+var txtOutput = document.querySelector("#output-text");
+
+var serverUrl = "https://api.funtranslations.com/translate/shakespeare.json?";
+
+function translateUrl(text) {
+    return serverUrl + "text=" + text
+};
+
+function errorHandler(error){
+    console.log("error occured" + error);
+    alert("There is a problem with the server. Try again later");
+}
 
 function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(translateUrl(inputText))
+        .then(respone => respone.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
     
-}
+};
+
+translateBtn.addEventListener("click", clickHandler)
